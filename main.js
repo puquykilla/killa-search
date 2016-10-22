05ebc8c2662f8a363b0c7925111df2f7d309ecd8
Oscar Sanchez <oscar_sanchez39@hotmail.com>
2016-10-22 12:06:15 -0500

    was standardized language to English.

--- main.js
+++ main.js
@@ -1,155 +1,144 @@
 (function() {
 	self.Assistant = function() {
-		/**
-		* @nombre_sistema se debe nombrar al inicio de cada frase que hablemos
-		*/
-		this.nombre_sistema = 'luna';
-		this.genero_sistema = 'femenino';
-		this.recognition;
-		this.recognizing = false;
+		// We can give the name of the system before any sentence
+		var self = this;
+		self.system_name = 'Luna';
+		self.system_gender = 'femenino';
+		self.recognition;
+		self.recognizing = false;
 	}
 
 	self.Assistant.prototype = {
-		/**
-		* Esta funcion formatea la respuesta para adecuarla con el nombre del sistema antes de cada palabra
-		*/
-		FormatearRespuesta: function(respuesta) {
-			var respuesta_formateada;
-			return respuesta_formateada = this.nombre_sistema + ' ' + respuesta;
+
+		// This function formats the response to conform with the system name before each word
+		responseFormat: function(response) {
+			var formated_response = null;
+			console.log(self.system_name);
+			return formated_response = self.system_name + ' ' + response;
 		},
-		/**
-		* Funcion que permite colocar el nombre del sistema dentro de las respuestas o claves.
-		*/
-		PrepararNombreSistema: function(value) {
-			return value.replace("nombre_sistema", this.nombre_sistema);
+
+		// Function that allows you to place the name of the system within the responses or keys.
+		systemNameFormat: function(value) {
+			return value.replace('system_name', self.system_name);
 		},
-		/**
-		* Funcion que permite colocar el genero del sistema dentro de las respuestas o claves.
-		*/
-		PrepararGeneroSistema: function(value) {
-			return value.replace("genero_sistema", this.genero_sistema);
+
+		// Function that allows you to place the genre system within the responses or keys.
+		genderSystemFormat: function(value) {
+			return value.replace('system_gender', self.system_gender);
 		},
-		HablarSistema: function(respuesta_sistema) {
-			var voz_sistema = new SpeechSynthesisUtterance();
-			var vocez = window.speechSynthesis.getVoices();
-			voz_sistema.voice = vocez[0];
-			voz_sistema.text = respuesta_sistema;
-		    window.speechSynthesis.speak(voz_sistema);
+
+		speak: function(system_response) {
+			var system_voice = new SpeechSynthesisUtterance();
+			var voices = window.speechSynthesis.getVoices();
+			system_voice.voice = voices[0];
+			system_voice.text = system_response;
+			window.speechSynthesis.speak(system_voice);
 		},
-		BuscarWikipedia: function(mensaje_entrante, respuesta_sistema) {
-			var Wikipedia = "http://es.wikipedia.org/w/api.php";
+
+		wikipediaSearch: function(incoming_message, system_response) {
+			var Wikipedia = 'http://es.wikipedia.org/w/api.php';
 			$.ajax({
-		        url: Wikipedia,
-		        dataType: "jsonp",
-		        data: {
-		            'action': "opensearch",
-		            'format': "json",
-		            'search': mensaje_entrante
-		        },
-		        success: function(data) {
-		        	if (data[2] != '') {
-		            	respuesta_sistema = data[2];
-		        	}
-		        	Assistant.prototype.HablarSistema(respuesta_sistema);
-		    	}
-		    });
+				url: Wikipedia,
+				dataType: 'jsonp',
+				data: {
+					'action': 'opensearch',
+					'format': 'json',
+					'search': incoming_message
+				},
+				success: function(data) {
+					if (data[2] != '') {
+						system_response = data[2];
+					}
+					Assistant.prototype.speak(system_response);
+				}
+			});
 		},
-		/**
-		* Esta funcion permite que el sistema nos de una respuesta leyendo una db en json
-		*/
-		Respuesta: function(mensaje_entrante) {
-			var respuesta_sistema;
-			var mensaje_entrante = mensaje_entrante.toLowerCase();
-			var JsonDB = "db.json";
-			// var Wikipedia = "https://es.wikipedia.org/w/api.php?action=opensearch&search=" + mensaje_entrante + "&limit=1&namespace=0&format=json";
-			var respuesta_default = false;
 
-			/**
-			* Leemos un archivo json donde estan las respuestas
-			*/
+		// This function allows the system to give us a response reading a json db
+		response: function(incoming_message) {
+			var system_response;
+			var incoming_message = incoming_message.toLowerCase();
+			var JsonDB = 'db.json';
+			var response_default = false;
+
+			// Read a json file where are the responses
 			$.getJSON( JsonDB, function( data ) {
-		      $.each( data.respuestas, function( clave, valor ) {
-
-		      	clave = Assistant.prototype.PrepararNombreSistema(clave);
-
-				if (mensaje_entrante === Assistant.prototype.FormatearRespuesta(clave) || mensaje_entrante === clave ) {
-					/**
-					* @nuevo_valor tendra la respuesta con el nombre del sistema reemplazado
-					*/
-					valor = Assistant.prototype.PrepararNombreSistema(valor);
-					valor = Assistant.prototype.PrepararGeneroSistema(valor);
-					respuesta_sistema = valor;
-					respuesta_default = false;
+			  $.each( data.responses, function( key, value ) {
+				if (incoming_message === Assistant.prototype.responseFormat(key) || incoming_message === key ) {
+					value = Assistant.prototype.systemNameFormat(value);
+					value = Assistant.prototype.genderSystemFormat(value);
+					system_response = value;
+					response_default = false;
 					return false;
 				} else {
-					respuesta_default = true;
-					respuesta_sistema = data.default;
+					response_default = true;
+					system_response = data.default;
 				}
-		      });
-		    })
+			  });
+			})
 			.done( function() {
-				/**
-				* Enviamos repsuesta por voz
-				*/
-				if (respuesta_default) {
-					Assistant.prototype.BuscarWikipedia(mensaje_entrante, respuesta_sistema);
+				// Send voice response
+				if (response_default) {
+					Assistant.prototype.wikipediaSearch(incoming_message, system_response);
 				} else {
-					Assistant.prototype.HablarSistema(respuesta_sistema);
+					Assistant.prototype.speak(system_response);
 				}
 			});
 		}
 	}
 })();
 
-/**
-* El Webkit fue testeado en googleChrome Versión 54.0.2840.59 (64-bit)
-*/
+
+// The Webkit was tested in GoogleChrome version 54.0.2840.59 (64-bit)
 var assistant = new Assistant();
+console.log(assistant.system_name);
 if (!('webkitSpeechRecognition' in window)) {
-	var msg = "¡API webkitSpeechRecognition not supported!"; 
+	var msg = '¡API webkitSpeechRecognition not supported!'; 
 	alert(msg);
 } else {
 	assistant.recognition = new webkitSpeechRecognition();
-	assistant.recognition.lang = "es-VE";
+	assistant.recognition.lang = 'es-VE';
 	assistant.recognition.continuous = false;
 	assistant.recognition.interimResults = true;
 
+	// This callback is called when the system recognition start
 	assistant.recognition.onstart = function() {
-		console.log("The system is ready and listening.");
+		console.log('The system is ready and listening.');
 	}
 
+	// This callback is called when the system processes words
 	assistant.recognition.onresult = function(event) {
 		for ( var i = event.resultIndex; i < event.results.length; i++ ) {
-			var palabra
+			var word = null;
 			if( event.results[i].isFinal ){
-				palabra = event.results[i][0].transcript;
-				document.getElementById("texto").value = palabra;
-				assistant.Respuesta(palabra);
+				word = event.results[i][0].transcript;
+				document.getElementById('texto').value = word;
+				assistant.response(word);
 			} else {
-				palabra += event.results[i][0].transcript;
+				word += event.results[i][0].transcript;
 			}
 		}
 	}
 
+	// This callback is called when the system throw a error
 	assistant.recognition.onerror = function(event) {
-		console.log("Speech recognition error detected: " + event.error);
-		console.log("Aditional information: " + event.error);
+		console.log('Speech recognition error detected: ' + event.error);
+		console.log('Aditional information: ' + event.error);
 	}
 
+	// This callback is called when the system finishes speaking
 	assistant.recognition.onend = function() {
 		assistant.recognizing = false;
-		document.getElementById("hablar").innerHTML = "Hablar";
-		console.log("terminó de escuchar, llegó a su fin");
+		document.getElementById('hablar').innerHTML = 'Hablar';
+		console.log('The system finished listening.');
 	}
 }
 
-/**
-* Esta funcion permite que podamos hablarle al sistema
-*/
-function Hablar() {
+// This function allows us to talk to the system
+function talk() {
 	if (assistant.recognizing == false) {
 		assistant.recognition.start();
 		assistant.recognizing = true;
-		document.getElementById("hablar").innerHTML = "Hablando";
+		document.getElementById('hablar').innerHTML = 'Hablando';
 	}
 }
